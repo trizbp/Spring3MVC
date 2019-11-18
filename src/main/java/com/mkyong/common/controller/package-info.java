13ada92a704ff164b1d51f3455e1d951db4063b6
trizbp <triz.p.m@gmail.com>
2019-11-18 13:55:05 +0100

    Create package-info.java

---
+++ src/main/java/com/mkyong/common/controller/package-info.java
@@ -0,0 +1,9 @@
+/** Domain classes used to produce the JSON and XML output for the RESTful services. 
+ * <p>
+ * These classes contain the JAXB annotations.
+ *
+ * @since 1.0
+ * @author jwhite
+ * @version 1.1
+ */
+package com.intertech.cms.domain;
