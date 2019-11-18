0dc9e1a7079c16494aee6dcd5b1c95a270c1173c
trizbp <triz.p.m@gmail.com>
2019-11-18 13:42:09 +0100

    Update HelloController.java

--- src/main/java/com/mkyong/common/controller/HelloController.java
+++ src/main/java/com/mkyong/common/controller/HelloController.java
@@ -1,5 +1,5 @@
 package com.mkyong.common.controller;
- 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
