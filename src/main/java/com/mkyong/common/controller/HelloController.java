136ebb9556177edb4a190b4035b54361158a80f2
trizbp <triz.p.m@gmail.com>
2019-11-18 13:43:59 +0100

    Update HelloController.java

--- src/main/java/com/mkyong/common/controller/HelloController.java
+++ src/main/java/com/mkyong/common/controller/HelloController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+/**
+ @author bpm
+ */
 @Controller
 @RequestMapping("/welcome")
 public class HelloController {
