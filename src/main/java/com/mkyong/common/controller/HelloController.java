44bec241e81ac99aae7b971e3e95f22220b07d92
trizbp <triz.p.m@gmail.com>
2019-11-18 13:44:54 +0100

    Update HelloController.java

--- src/main/java/com/mkyong/common/controller/HelloController.java
+++ src/main/java/com/mkyong/common/controller/HelloController.java
@@ -16,7 +16,7 @@ public class HelloController {
  @param model
 */
 @RequestMapping(method = RequestMethod.GET)
-public String printWelcome(final ModelMap model) {
+public String printWelcome(@param final ModelMap model) {
 	model.addAttribute("message",	"Spring Security Hello World");
 	return "hello";
  }
